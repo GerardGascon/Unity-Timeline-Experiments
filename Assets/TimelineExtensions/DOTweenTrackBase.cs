3ee8001bb4ecd61015f96936d30d7438315bfd26
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2023-06-25 13:30:08 +0200

    Revert "Cleanup"
    This reverts commit 3e41a08bd51fe89ff789e77b04a37dc5a6595afb.

--- Assets/TimelineExtensions/DOTweenTrackBase.cs
+++ Assets/TimelineExtensions/DOTweenTrackBase.cs
@@ -1,4 +1,3 @@
-using DG.Tweening;
 using UnityEditor;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -7,41 +6,14 @@ using UnityEngine;
 namespace TimelineExtensions {
 	[TrackColor(0, 1, 0)]
 	public abstract class DOTweenTrackBase<T> : TrackAsset where T : class {
-		protected class DOTweenMixer : PlayableBehaviour {
-			static void ProcessInput(float progress, Tween tween) {
-				tween.Goto(progress * tween.Duration());
-			}
-		
-			public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
-				IterateInput(playable);
-			}
-
-			// Tool
-			void IterateInput(Playable playable) {
-				int inputCount = playable.GetInputCount();
-				for (int i = 0; i < inputCount; i++) {
-					if (playable.GetInputWeight(i) <= 0) continue;
-
-					ScriptPlayable<DOTweenBehaviourBase> derivedPlayable = 
-						(ScriptPlayable<DOTweenBehaviourBase>)playable.GetInput(i);
-					DOTweenBehaviourBase behaviour = derivedPlayable.GetBehaviour();
-
-					if (behaviour == null) continue;
-					float progress = (float)(derivedPlayable.GetTime() / derivedPlayable.GetDuration());
-
-					ProcessInput(progress, behaviour.tween);
-				}
-			}
-		}
-		
 		public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount) {
 			T binding = go.GetComponent<PlayableDirector>().GetGenericBinding(this) as T;
  
 			foreach (TimelineClip c in GetClips())
 				((DOTweenClipBase<T>)c.asset).Target = binding;
 			
-			ScriptPlayable<DOTweenMixer> playable =
-				ScriptPlayable<DOTweenMixer>.Create(graph, inputCount);
+			ScriptPlayable<DOTweenMixerBase> playable =
+				ScriptPlayable<DOTweenMixerBase>.Create(graph, inputCount);
 			
 			ProcessPlayable(playable);
  
